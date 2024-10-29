8a3976a99d6a232012aabd4dbaa6a97a1f858a52
agheessam <agheessamuel1@gmail.com>
2024-10-29 15:47:03 +0530

    changes

--- main.tf
+++ main.tf
@@ -10,6 +10,6 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"  # North Virginia region
-  access_key = AKIA2UC3AGHJC53FVU4G 
-  secret_key = WvJRgHdSRRnrSZscDmjpXmILJRz+qYaUvZV3dAS
+  access_key =
+  secret_key = 
 }
